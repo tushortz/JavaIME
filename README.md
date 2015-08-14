04301488bc387ff0f284cba29f58d71de2e23a64
Taiwo Kareem <taiwo.kareem36@gmail.com>
2015-08-14 14:58:22 +0100

    first release

---
+++ README.md
@@ -0,0 +1,75 @@
+
+
+# Java Import Made Easy (JavaIME)
+
+## About
+**JavaIME** is a Sublime text package that makes importing any java package easy.
+
+## Installation
+s
+Open `Tools -> Command Palette...`. Search for `Package Control: Install Package` and click enter. Wait for
+the available packages to show up and then search for `JavaIME`. Click enter and the package should
+be installed.
+
+### Using Git
+
+Locate your Sublime Text 2 `Packages` directory by using the menu item `Preferences -> Browse Packages...`.
+
+While inside the `Packages` directory, clone the theme repository using the command below:
+
+    git clone https://github.com/tushortz/JavaIME.git
+
+### Download Manually
+
+* Download the files using the GitHub .zip download option
+* Unzip the files
+* Copy the folder to your Sublime Text 2 `Packages` directory
+
+
+optionally
+
+###For windows
+#### for Sublime text 2
+copy the **JavaIME** folder and paste the file into the location of your default packages. It should be found by opening sublime text then clicking `preferences -> Packages`
+
+
+#### For Sublime text 3
+copy the **JavaIME** folder and paste the file into the following location.
+
+##### File location
+
+> **Note:** You will need to *show hidden files* first
+
+> C:\Users\User name\AppData\Local\Sublime Text 3\Cache
+
+## How to Use
+
+On a Java file in sublime text, click on the line where you want to import the Java Package then type its name all in uppercase and suggestions should follow.
+
+### Example
+To import the *java.utils.Collections package*, type **COLLECTIONS** in *(uppercase)*. Suggestions should already be popping up then press *TAB* to select. The package will be automatically imported.
+
+### Screenshot
+![Java Import Made Easy {JavaIME}](./preview.png)
+
+## Contributing
+
+All contributions are welcome. 
+
+## License
+© 2015 Taiwo Kareem | taiwo.kareem36@gmail.com.
+
+This is free software. It is licensed under the MIT License. Feel free to use this in your own work. However, if you modify and/or redistribute it, please attribute me in some way, and it would be great if you distribute your work under this or a similar license, but it's not required.
+
+## Acknowledgements
+I'd first like to say a very big thank you to God my creator. Without him, this wouldn't be possible.
+
+## Donating
+> If you would like to show appreciation for the efforts and time spent making this, Please contact me on my email **taiwo.kareem36@gmail.com**
+
+
+
+
+
+
+
---
+++ java/createpackage.py
@@ -0,0 +1,99 @@
+import sys
+import os, os.path
+
+
+before = (len([(str(name)) for name in os.listdir('.') if os.path.isfile(name)]))
+
+######################################################################################
+# Change thefolder path
+maindir = "C://Users//Taiwo Kareem//Desktop//src//"
+thefolder = maindir + "java//util"
+######################################################################################
+
+dir = os.listdir(thefolder)
+
+links = (thefolder.replace("//", ".")[34:])
+pathabbr = links.rsplit('.')
+
+# for i in pathabbr:
+#     if len(i) < 1:
+#         pathabbr.remove(i)
+pathabbrev = ""
+
+# print(pathabbr)
+#########################################################
+mainpackage = pathabbr[0]
+###########################################################
+# print(mainpackage)
+# print(links)
+
+for i in pathabbr:
+    pathabbrev +=i[0]
+
+pathabbrev += "-"           
+# print (pathabbrev)
+# print (pathabbr)
+
+PACK = []
+for i in dir:
+    PACK.append(i[:-5])
+
+# print(PACK)
+
+packageshortname = []
+for x in PACK:
+    abbreviate = ""
+    for i in x:
+        if i.isupper():
+            abbreviate += i
+    packageshortname.append(pathabbrev + abbreviate + str(PACK.index(x)+1))
+
+# print(packageshortname)
+
+##################################################
+#Main write code
+def write(links, PACK, pathabbr, packageshortname):
+    print('Creating new snippet file') 
+
+    line1 = ("\n<snippet>\n")
+    line2 = ("\t<content><![CDATA[\n")
+
+    # Edit
+    line3 = ("import %s.%s;\n" %(links,PACK))
+    line4 = ("]]></content>\n")
+    line5 = ("\t<tabTrigger>%s</tabTrigger>\n"%(PACK.upper()))
+    line6 = ("\t<scope>source.java</scope>\n")
+    line7 = ("\t<description>%s Package</description>\n"%(pathabbr.title()))
+    line8 = ("</snippet>")
+
+    print("writing data commenced\n")
+    lines = [line1, line2, line3, line4, line5, line6, line7, line8]
+
+    #Edit
+    filename = packageshortname + '.sublime-snippet'  # Name of file 
+    filename = open(filename,'w')  
+
+    for x in lines:
+    	filename.write(x)
+    filename.close()
+    print(str(filename) + ' has been created' )
+
+
+
+x = 0
+while x < len(PACK):
+    write(links, PACK[x], pathabbr[0], packageshortname[x])
+    x+=1
+
+
+
+# print(links)
+# print(dir)
+
+# # simple version for working with CWD
+after = (len([(str(name)) for name in os.listdir('.') if os.path.isfile(name)]))
+
+print(after - before , "files created")
+print("Total files:", after)
+# print (abbreviate)
+
